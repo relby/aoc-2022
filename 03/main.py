2f4d68810a03e3eceddd509b17b6c400984e4364
relby <kudinov.nikita@gmail.com>
2022-12-03 17:55:32 +0300

    [03] Complete second part

--- 03/main.py
+++ 03/main.py
@@ -1,6 +1,7 @@
 from string import ascii_lowercase, ascii_uppercase
 
 
+# Part 1
 def priority(item: str):
     if item in ascii_lowercase:
         return ord(item) - ord('a') + 1
@@ -30,10 +31,29 @@ def part1_functional(rucksacks: list[str]) -> int:
     return sum(priority(get_common(rucksack)) for rucksack in rucksacks)
 
 
+# Part 2
+def chunks(lst: list, n: int) -> list[list]:
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def part2(rucksacks: list[str]) -> int:
+    items: list[str] = []
+    for group in chunks(rucksacks, 3):
+        items.append(
+            tuple(set(group[0])
+                  & set(group[1])
+                  & set(group[2]))[0]
+        )
+    return sum(priority(item) for item in items)
+
+
 def main() -> None:
     with open('input.txt') as input_file:
-        rucksacks = input_file.readlines()
+        rucksacks = [line.strip() for line in input_file.readlines()]
     print('Part 1:', part1(rucksacks))
+    print('Part 2:', part2(rucksacks))
 
 
 if __name__ == '__main__':
